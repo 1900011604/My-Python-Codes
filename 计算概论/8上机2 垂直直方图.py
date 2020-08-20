c876a48de8870d02917c40ec5c2cf22567eea8ac
1900011604 <1900011604@pku.edu.cn>
2020-08-20 23:29:56 +0800

    update 8sj2

--- 计算概论/8上机2 垂直直方图.py
+++ 计算概论/8上机2 垂直直方图.py
@@ -1,37 +1,37 @@
-txt=''
-a=[]
+import copy
+
+txt = ''
+a = []
 for i in range(4):
-    txt+=input()
-for i in range(65,91):
+    txt += input()
+for i in range(65, 91):
     a.append(txt.count(chr(i)))
 
-import copy
-b=copy.deepcopy(a)
+b = copy.deepcopy(a)
 c = copy.deepcopy(a)
 
-empty=[]
+empty = []
 for i in range(26):
-    empty+=[' ']
-emptystr=''
+    empty += [' ']
+emptystr = ''
 
-m=max(b)
+m = max(b)
 for i in range(m):
     for i in range(26):
-        a[i]=a[i]-m+1
-        if a[i]<=0:
-            a[i]=0
-    #print(a) 真正要用到的
+        a[i] = a[i]-m+1
+        if a[i] <= 0:
+            a[i] = 0
     for i in range(26):
-        if a[i]==1:
-            empty[i]='* '
+        if a[i] == 1:
+            empty[i] = '* '
         else:
-            empty[i]='  '
-        emptystr+=empty[i]
+            empty[i] = '  '
+        emptystr += empty[i]
     print(emptystr)
-    emptystr=''
+    emptystr = ''
     for i in range(26):
-        a[i]=c[i]-a[i]
+        a[i] = c[i]-a[i]
     c = copy.deepcopy(a)
-    m-=1
+    m -= 1
 
 print('A B C D E F G H I J K L M N O P Q R S T U V W X Y Z ')
